ce3a83231248dbb0328e7b16b50a24143522d3f9
AEPM <kamuizero@hotmail.com>
2017-06-26 13:30:29 +0900

    Pruebas con validator y session

--- 08-validation-sessions/routes/index.js
+++ 08-validation-sessions/routes/index.js
@@ -3,14 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Form Validation', success: false, errors: req.session.errors});
+  res.render('index', { title: 'Form Validation', success: req.session.success, errors: req.session.errors});
   req.session.errors = null;
+  req.session.success = null;
+
 });
 
 
 router.post('/submit', function(req, res, next) {
     //Chequear la validez
-    req.check('email');
+    req.check('email', 'Direccion de correo invalida').isEmail(); //Este nombre tiene que ser el mismo que el NOMBRE del elemento que queremos validar
+    req.check('password', 'Password es invalido').isLength({min: 4}).equals(req.body.confirmPassword);
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+        req.session.errors = errors;
+        req.session.success = false;
+    } else {
+        req.session.success = true;
+    }
+    res.redirect('/');
 });
 
 module.exports = router;
